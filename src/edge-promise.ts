5fa56a2182bbc2b15b6bab92042cb0b23a732b9b
Mark <markberry867@gmail.com>
2018-05-18 00:57:02 -0500

    Minor updates

--- src/edge-promise.ts
+++ src/edge-promise.ts
@@ -1,16 +1,18 @@
-const edge = require((process.versions['electron']) ? 'electron-edge' : 'edge')
+// const edge = require((process.versions['electron']) ? 'electron-edge' : 'edge')
 
-const edgePromise = (src) => {
-  const f = edge.func(src)
-  return (obj) => {
-    return new Promise((resolve, reject) => {
-      f(obj, (e, ret) => {
-        if (e) { return reject(e) }
-        resolve(ret)
-      })
-    })
-  }
-}
+// const edgePromise = (src) => {
+//   const f = edge.func(src)
+//   return (obj) => {
+//     return new Promise((resolve, reject) => {
+//       f(obj, (e, ret) => {
+//         if (e) { return reject(e) }
+//         resolve(ret)
+//       })
+//     })
+//   }
+// }
+
+const edgePromise = Promise.reject(new Error('edge and electron-edge dependency removed'))
 
 module.exports = edgePromise
 module.exports.func = edgePromise
